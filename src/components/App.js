54d04e86e67cd7ab580042fc2fa0999c16dba383
tintoll <blue@tigrison.com>
2017-01-03 13:06:00 +0900

    리액트 3장 공부 내용 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
-    render() {
-
+    sayHey() {
+        alert('hey');
+    }
+    render(){
+        let text = 'Dev-Server';
+        let pStyle = {
+            color: 'aqua',
+            backgroundColor: 'black'
+        };
         return (
-            <h1 > Hello React Skeleton11 </h1 >
+            <div>
+                <h1>Hello Lee</h1>
+                <h2> Welcome to {text}</h2>
+                <button onClick={this.sayHey}>Click Me</button>
+
+                <p style = {pStyle}>{1 == 1 ? 'True' : 'False'}</p>
+                {/*jsx에서는 if-else가 없어서 위와 같이 처리한다. */}
+            </div>
         );
     }
 }
